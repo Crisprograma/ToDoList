aa53b9bc979977c9f864eb85f2c7f42ee6e0dcf6
Crisprograma <123573781+Crisprograma@users.noreply.github.com>
2023-04-23 16:12:14 +0000

    Listado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [input, setInput] = useState("");
+  const [tareas, setTareas] = useState([]);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleAdd = () => {
+    const tareaSinEspacios = input.trim();
+    if (tareaSinEspacios !== "") {
+      setTareas([...tareas, tareaSinEspacios]);
+    }
+    setInput("");
+  };
+
+  const handleDelete = (index) => {
+    const tareasRestantes = tareas.filter((tarea, i) => i !== index)
+    setTareas(tareasRestantes)
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1>To Do List</h1>
+      <input
+        type="text"
+        value={input}
+        placeholder={isInputFocused ? "" : "Indica tu tarea"}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleAdd();
+          }
+        }}
+        onFocus={() => setIsInputFocused(true)}
+        onBlur={() => setIsInputFocused(false)}
+      />
+      <ul>
+        {tareas.map((tarea, index) => (
+          <li key={index}>
+            {" "}
+            {tarea}{" "}
+            <button
+              type="button"
+              className="eliminar btn-close"
+              aria-label="Close"
+              onClick={() => handleDelete(index)}
+            ></button>
+          </li>
+        ))}
+      </ul>
+      <footer>{tareas.length === 0 ? '' : `${tareas.length} items left`}</footer>
+    </div>
+  );
 };
 
 export default Home;
+
